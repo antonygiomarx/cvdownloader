fbbd7732b54191e94bf00ebd8c391f2f7bc41191
Antony Giomar <antonygiomarx@gmail.com>
2023-02-20 12:52:53 -0600

    chore: improve import :pencil2:

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
 export class App {
